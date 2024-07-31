eca8024f4b74d98c80b7a44f36854ac6ab6d0cdd
danielyatab <yataco-126@hotmail.com>
2024-07-31 15:22:39 -0500

    Update favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="bg-slate-900 h-screen w-screen flex justify-center items-center">
-      <div className="text-center space-y-4">
+      <div className="text-center flex flex-col gap-4">
         <h1 className="text-3xl font-semibold">Hello Memory</h1>
-        <Link href={"/memory"} className="bg-teal-700 px-2 py-1 rounded-lg">
+        <Link
+          href={"/memory"}
+          className="bg-teal-700 px-2 py-1 rounded-lg font-semibold"
+        >
           Go Memory
         </Link>
       </div>
